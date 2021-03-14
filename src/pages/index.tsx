e5bca45adb9994fbd08bcaae59089784e6de0eb9
hktysk <komikansougou@gmail.com>
2021-03-14 20:54:59 +0900

    feat(index): ログイン機能を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import Template from '../components/templates/Index';
 import { Button } from '../types/login.type';
+import {
+  googleSignIn,
+  gitHubSignIn,
+  twitterSignIn
+} from '../service/auth.service';
 
 const buttons: Button[] = [
   {
     icon: '/assets/img/sns-icons/google.png',
     text: 'Google',
-    onClick: () => {}
+    onClick: googleSignIn
   },
   {
     icon: '/assets/img/sns-icons/github.png',
     text: 'Github',
-    onClick: () => {}
+    onClick: gitHubSignIn
   },
   {
     icon: '/assets/img/sns-icons/twitter.png',
     text: 'Twitter',
-    onClick: () => {}
+    onClick: twitterSignIn
   },
 ];
 
